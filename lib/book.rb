6e7321f97bc573552d64fcb3c141658dcda708a9
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-07-11 14:11:35 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -5,7 +5,11 @@ class Book
 attr_accessor  :author, :page_count, :genre
 attr_reader :title
 def turn_page
+<<<<<<< HEAD
   puts "Flipping the page...wow, you read fast!"
+=======
+  puts "turn a page"
+>>>>>>> 7618dcc728a4573e5619509d620d2e5806ce2cc3
 end
 end
 
